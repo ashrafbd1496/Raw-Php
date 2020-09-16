1b54ead54485b6e310f99e7bfad6c4a73785bdc7
md ashraf uddin <asraf1496@gmail.com>
2020-09-16 12:44:57 +0600

    Update datatypes.php

--- datatypes.php
+++ datatypes.php
@@ -1,6 +1,33 @@
 <?php 
 
-echo "String '\n'
-inteter";
+function resultSystem($mark){
+
+
+
+	if ($mark >= 0 && $mark <= 32) {
+		$grade = 'F';
+	}elseif ($mark >= 33 && $mark <= 39) {
+		$grade = 'D';
+	}elseif ($mark >= 40 && $mark <= 49) {
+		$grade = 'C';
+	}elseif ($mark >= 50 && $mark <= 59) {
+		$grade = 'B';
+	}elseif ($mark >= 60 && $mark <= 69) {
+		$grade = 'A-';
+	}elseif ($mark >= 70 && $mark <= 79) {
+		$grade = 'A';
+	}elseif ($mark >= 80 && $mark <= 100) {
+		$grade = 'A+';
+	}else{
+		$grade = 'Invalid';
+	}
+
+	return $grade; 
+
+}
+
+echo resultSystem(50);
+
+
 
  ?>
