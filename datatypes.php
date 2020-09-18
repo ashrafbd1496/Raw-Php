80246d95b538a2bd4fbc8445a29870a33aa206be
md ashraf uddin <asraf1496@gmail.com>
2020-09-18 20:48:43 +0600

    added types

--- datatypes.php
+++ datatypes.php
@@ -1,6 +1,18 @@
 <?php 
 
-echo "String '\n'
-inteter";
+//nl2br used for new line
+
+echo "<h2>Php Data Types</h2>";
+echo "<br>";
+echo nl2br("String\n
+Integer\n
+Float (floating point numbers - also called double)\n
+Boolean\n
+Array\n
+Object\n
+NULL\n
+Resource
+");
+
 
  ?>
