743d7b561de94d04e82bf833bc90fdcd158bf2da
md ashraf uddin <asraf1496@gmail.com>
2020-09-20 06:44:59 +0600

    array_splice() function

---
+++ arraySplice.php
@@ -0,0 +1,24 @@
+<?php 
+
+ $food = ['alu','potol','kumra','piaz','rosun','pepe'];
+
+
+
+//array_splice Remove elements from an array and replace it with new elements:
+
+ array_splice($food, 2,2,['ada','zira']);
+
+
+echo "<pre>";
+print_r($food);
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Array_slice() function- Remove elements from an array and replace it with new elements:
+
+";
+
+
+ ?>
