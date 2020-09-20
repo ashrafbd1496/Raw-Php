b383e18dfd1b4926585dfc720e0ae449a642835d
md ashraf uddin <asraf1496@gmail.com>
2020-09-20 10:30:35 +0600

    Data receive from a form

---
+++ form_data_receive.php
@@ -0,0 +1,21 @@
+<?php 
+
+	echo "<pre>";
+	print_r($_POST);
+	echo "</pre>";
+
+
+ ?>
+
+
+
+
+
+
+		<form action="" method="POST">
+			<input type="text" name="name" placeholder="Name">
+			<input type="text" name="email" placeholder="Email">
+			<input type="text" name="age" placeholder="Age">
+			<input type="text" name="password" placeholder="Password">
+			<input name ="signup" type="submit" value="Sign Up">
+		</form>
