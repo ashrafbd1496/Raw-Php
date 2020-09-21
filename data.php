d6b4841efbbcac3fe7f229509282a04bfb29cfb0
md ashraf uddin <asraf1496@gmail.com>
2020-09-21 23:17:34 +0600

    form data received at separate file data.php

---
+++ data.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+	if (isset($_POST['signup'])) {
+		echo $name = $_POST['name'];
+		echo $email = $_POST['email'];
+		echo $age = $_POST['age'];
+		echo $password = $_POST['password'];
+	}
+
+
+ ?>
---
+++ form_action.php
@@ -0,0 +1,13 @@
+
+
+
+		<form action="data.php" method="POST">
+			<input type="text" name="name" placeholder="Name">
+			<input type="text" name="email" placeholder="Email">
+			<input type="text" name="age" placeholder="Age">
+			<input type="text" name="password" placeholder="Password">
+			<input name ="signup" type="submit" value="Sign Up">
+		</form>
+
+
+ 
