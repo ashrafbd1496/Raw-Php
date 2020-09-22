b1673cf26397cd248c63e41fe306f8932c630df6
ashrafuddin1496 <asraf1496@gmail.com>
2020-09-23 00:43:26 +0600

    pi,abs,cos,sin etc math function added

---
+++ math_functions.php
@@ -0,0 +1,67 @@
+<?php 
+
+echo pi();
+
+echo "<br>";
+
+// round function kono float number k round figure a convert kore 
+echo round(5.6);
+echo "<br>";
+
+echo round(5.2);
+
+// floor function sudhu nicher dike nea ese round kore 
+echo "<br>";
+echo floor(7.2);
+
+echo "<br>";
+echo floor(7.7);
+
+
+// The pow() function returns x raised to the power of y.
+echo "<br>";
+echo pow(2, 5);
+
+// Return the cosine of different numbers:
+
+echo "<br>";
+echo cos(0);
+
+
+echo "<br>";
+echo sin(90);
+
+
+echo "<br>";
+echo cos(90);
+
+//Return the tangent of different numbers:
+
+
+echo "<br>";
+echo tan(45);
+
+//show the max value form these numbers
+echo "<br>";
+echo max(3,5,8,9,74,51,53,2,60,41);
+
+
+//show the min value form these numbers
+echo "<br>";
+echo min(3,85,12,60,5,32,40);
+
+
+//show the absolute value mean minus will be converted to plus
+//Return the absolute value of different numbers:
+echo "<br>";
+echo abs(-75);
+
+echo "<br>";
+echo abs(3.5);
+
+echo "<br>";
+echo abs(-6.5);
+
+
+
+ ?>
