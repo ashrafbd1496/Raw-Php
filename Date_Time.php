0ffe193ce8e3efa05da6a690ccf0f1b050579efa
ashrafuddin1496 <asraf1496@gmail.com>
2020-09-22 12:54:49 +0600

    date added in different format

--- Date_Time.php
+++ Date_Time.php
@@ -2,5 +2,28 @@
 
 echo time();
 
+echo "<br>";
+
+
+echo date('d m y');
+
+//to show date time in full format , need to use capital letter
+
+echo "<br>";
+echo date('D M Y');
+
+echo "<br>";
+echo date('d m Y');
+
+echo "<br>";
+echo date('d F Y');
+
+echo "<br>";
+echo date('g : i : A');
+
+echo "<br>";
+echo date('H : i : A');
+
+
 
  ?>
