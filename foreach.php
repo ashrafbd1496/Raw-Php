52449e3dee7b94c96dc398c0742a9a2953879fbd
ashrafuddin1496 <asraf1496@gmail.com>
2020-09-27 16:10:31 +0600

    foreach loop

---
+++ foreach.php
@@ -0,0 +1,27 @@
+<?php 
+
+//for each loop used only for array
+
+for ($i=1; $i <= 10 ; $i++) { 
+
+		// echo $i . "<br>". "<br>";
+
+	//multiplication table(namta)
+
+	//to rwite math multiple symble(alt+0215)
+
+
+	// echo "2 × $i =" . (2 * $i) . "<br>" . "<br>";
+
+	}
+
+
+
+	
+	$fol = ['aam','jaam','kola','lichu','pepe','peara','kathal'];
+
+	foreach ($fol as $ful) {
+		echo "Foler name = " . $ful . "<br>";
+	}
+	
+ ?>
