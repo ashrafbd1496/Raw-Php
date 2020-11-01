00d825f8c75581727d9984e34f3cc8030bff70d9
md ashraf uddin <asraf1496@gmail.com>
2020-11-01 12:21:13 +0600

    Difference between return and echo

---
+++ return.php
@@ -0,0 +1,15 @@
+<?php
+function sayHelloLater(){
+    return "This is return";
+}
+
+function sayGoodbyeNow(){
+    echo "This is echo <hr>";
+}
+
+$hello = sayHelloLater(); // "Hello" returned and stored in $hello 
+$goodbye = sayGoodbyeNow(); // "Goodbye" is echo'ed and nothing is returned
+
+echo $hello; // "Hello" is echo'ed
+echo $goodbye; // nothing is echo'ed
+?>
