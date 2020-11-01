43e4e1e1c8dcfe7d9f3f8235ae0158578c4bac31
md ashraf uddin <asraf1496@gmail.com>
2020-11-01 12:50:50 +0600

    edited

--- return.php
+++ return.php
@@ -12,4 +12,8 @@ $goodbye = sayGoodbyeNow(); // "Goodbye" is echo'ed and nothing is returned
 
 echo $hello; // "Hello" is echo'ed
 echo $goodbye; // nothing is echo'ed
+
+echo "<hr>";
+echo $goodbye; // It has nothig output value, because its already blank
+
 ?>
